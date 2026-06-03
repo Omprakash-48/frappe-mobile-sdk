adb00f0b15cfbb32fd5e8921ce30a687fe465592
ankitDhwani <ankit@dhwaniris.com>
2026-06-03 18:02:17 +0530

    fix: resolve label overflows (#52) and multiple tickers crash (#72)

--- lib/src/ui/widgets/form_builder.dart
+++ lib/src/ui/widgets/form_builder.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import '../../models/doc_type_meta.dart';
@@ -251,7 +252,7 @@ class _FormColumn {
 }
 
 class _FrappeFormBuilderState extends State<FrappeFormBuilder>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   late GlobalKey<FormBuilderState> _formKey;
   late final FieldFactory _fieldFactory;
   LinkFieldCoordinator? _linkFieldCoordinator;
@@ -267,6 +268,8 @@ class _FrappeFormBuilderState extends State<FrappeFormBuilder>
   /// builder is a top-level form (not a child-row form).
   Map<String, dynamic> get effectiveParentFormData =>
       widget.parentFormData ?? _formData;
+
+  TabController get tabController => _tabController;
 
   void _attachTabControllerListener() {
     _tabController.addListener(() {
@@ -1181,8 +1184,9 @@ class _FrappeFormBuilderState extends State<FrappeFormBuilder>
   @override
   void didUpdateWidget(FrappeFormBuilder oldWidget) {
     super.didUpdateWidget(oldWidget);
-    if (oldWidget.initialData != widget.initialData ||
-        oldWidget.meta != widget.meta) {
+    final initialDataChanged = !mapEquals(oldWidget.initialData, widget.initialData);
+    final metaChanged = oldWidget.meta.name != widget.meta.name;
+    if (initialDataChanged || metaChanged) {
       _progressSubscription?.cancel();
       _linkFieldCoordinator?.dispose();
       _linkFieldCoordinator = null;
